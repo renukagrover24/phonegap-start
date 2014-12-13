ebb6910cd70f89be8c4118b5cf0ed83d5bb0d2fa
renukagrover24 <renuka.grover24@gmail.com>
2014-12-13 13:44:58 +0530

    triangle are app

--- www/js/index.js
+++ www/js/index.js
@@ -27,6 +27,7 @@ var app = {
     // 'load', 'deviceready', 'offline', and 'online'.
     bindEvents: function() {
         document.addEventListener('deviceready', this.onDeviceReady, false);
+	document.getElementById('btnCalculate').addEventListener('click', this.onCalculate, false);
     },
     // deviceready Event Handler
     //
@@ -45,5 +46,21 @@ var app = {
         receivedElement.setAttribute('style', 'display:block;');
 
         console.log('Received Event: ' + id);
-    }
+    },
+
+onCalculate: function() {
+var b = document.getElementById('base').value;
+var h = document.getElementById('height').value;
+if (b.length == 0 || h.length == 0)
+document.getElementById('res').innerHTML = "ERROR: Please fill all the fields.";
+else if (b == 0 || h == 0)
+document.getElementById('res').innerHTML = "ERROR: Please enter valid values."; 
+else {
+var x = parseInt(b);
+var y = parseInt(h);
+var area = 0.5 * x * y;
+document.getElementById('res').innerHTML = "Area = " + area;
+}
+}
+
 };
